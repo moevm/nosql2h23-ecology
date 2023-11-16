3b91a285c9a16cbcb8326415642a42692ef666ed
Saprigenie <Slava.paramonov.2002@yandex.ru>
2023-11-16 15:38:19 +0300

    fix: fixed zoom string error at server

--- worker/app/config.py
+++ worker/app/config.py
@@ -3,5 +3,5 @@ from os import environ
 MONGO_URI = environ.get('MONGO_URI', "mongodb://localhost:27017/db")
 REDIS_URI = environ.get('REDIS_URI', "redis://localhost:6379/0")
 
-MIN_ZOOM = environ.get('MIN_ZOOM', 1)
-MAX_ZOOM = environ.get('MAX_ZOOM', 15)
+MIN_ZOOM = int(environ.get('MIN_ZOOM', 1))
+MAX_ZOOM = int(environ.get('MAX_ZOOM', 15))
