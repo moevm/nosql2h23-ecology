6fdb89829d2d82f6c162a623fb877da011828fa8
Давыдов Михаил <71800457+dart-mih@users.noreply.github.com>
2023-11-17 00:24:01 +0300

    update maps db

--- server/app/routes/maps_bp.py
+++ server/app/routes/maps_bp.py
@@ -20,89 +20,74 @@ maps_bp = Blueprint('maps_bp', __name__, url_prefix="/maps")
 
 
 @maps_bp.route('/', methods=['GET'])
+# delete excess
 def get_images_list():
     maps = []
-    for img in db.maps.find({}):
+    for one_map in maps_fs.files.find({}):
         maps.append({
-            "id": str(img["_id"]),
-            "name": img["name"],
-            'uploadDate': img["upload_date"],
-            'size': maps_fs.find_one({'_id': img["fs_id"]}).length,
-            "ready": img["ready"],
-            "sliced": img["sliced"]
+            "id": str(one_map["_id"]),
+            "name": one_map["name"],
+            "update": one_map["update"],
+            "location": one_map["location"],
+            "tile_map_resource": one_map["tile_map_resource"],
+            "size": one_map["size"],
+            "ready": one_map["ready"],
+            "sliced": one_map["sliced"]
         })
 
     return maps
 
-''' УБРАТЬ '''
+
 @maps_bp.route('/tile_map_resource/<string:img_id>', methods=['GET'])
 def index(img_id):
-    tile_map_resource = db.images.find_one(ObjectId(img_id))["tile_map_resource"]
+    tile_map_resource = maps_fs.files.find_one(ObjectId(img_id))["tile_map_resource"]
     if tile_map_resource is None:
         abort(404)
     else:
-        return db.images.find_one(ObjectId(img_id))["tile_map_resource"]
+        return maps_fs.files.find_one(ObjectId(img_id))["tile_map_resource"]
 
 
 @maps_bp.route('/upload_map', methods=['POST'])
 def add_map():
     new_map = request.files['map']
-    file_id = maps_fs.put(new_map, filename=new_map.filename, chunk_size=256 * 1024)
-    img_name = request.form.get('name')
-    item = {
-        "tile_map_resource": None,
-        "fs_id": file_id,
-        "objects": [],
-        "upload_date": str(arrow.now().to('UTC')),
-        "detect_date": "",
-        "name": img_name,
-        'ready': False,
-        'sliced': False
-    }
-    
-    result = db.images.insert_one(item)
-    img_id = result.inserted_id
+    file_id = maps_fs.put(new_map, filename=request.form.get('name'),  chunk_size=256 * 1024,
+                          metadata={"location": "something",
+                                    "tile_map_resource": "something too",
+                                    "size": "new_map.size() ?",
+                                    "ready": False,
+                                    "sliced": False})
 
-    slice.delay(str(img_id))
-    process_image.delay(str(img_id))
+    slice.delay(str(file_id))
+    process_image.delay(str(file_id))
 
     return jsonify({'message': 'Image added successfully'})
 
 
-@maps_bp.route('/delete_image/<string:img_id>', methods=['DELETE'])
-def delete_image(img_id):
-    image_info = db.images.find_one(ObjectId(img_id))
-    if (image_info):
-        fs_id = image_info["fs_id"]
-
-        db.images.delete_one({"_id": ObjectId(img_id)})
-
-        maps_fs.delete(fs_id)
-        for tile in tiles_fs.find({"image_id": ObjectId(img_id)}):
+@maps_bp.route('/delete_map/<string:map_id>', methods=['DELETE'])
+def delete_map(map_id):
+    map_info = maps_fs.files.find_one(ObjectId(map_id))
+    if map_info:
+        maps_fs.delete(ObjectId(map_id))
+        for tile in tiles_fs.files.find({"image_id": ObjectId(map_id)}):
             tiles_fs.delete(tile._id)
 
-        socketio.emit("images", get_images_list())
+        socketio.emit("images", get_images_list()) #update sockets
         return jsonify({'message': 'Image deleted successfully'})
     return 'OK'
 
 
 # Маршрут для leaflet-а, возвращает кусочки для отображения.
-@maps_bp.route("/tile/<string:img_id>/<int:z>/<int:x>/<int:y>", methods=['GET'])
-def get_tile(img_id, z, x, y):
-    tile = tiles_fs.find_one({'image_id': ObjectId(img_id), 'z': z, 'x': x, 'y': y})
+@maps_bp.route("/tile/<string:map_id>/<int:z>/<int:x>/<int:y>", methods=['GET'])
+def get_tile(map_id, z, x, y):
+    tile = tiles_fs.files.find_one({'map_id': ObjectId(map_id), 'z': z, 'x': x, 'y': y})
     if tile:
         return send_file(io.BytesIO(tile.read()), mimetype='image/png')
     else:
         return 'OK'
 
 
-@maps_bp.route('/<string:img_id>', methods=['GET'])
-def get_image(img_id):
-    image_info = db.images.find_one(ObjectId(img_id))
-    image_file = tiles_fs.get(image_info["fs_id"])
-    return send_file(io.BytesIO(image_file), mimetype='image/tiff')
+@maps_bp.route('/<string:map_id>', methods=['GET'])
+def get_image(map_id):
+    maps_file = maps_fs.files.find_one(ObjectId(map_id)) #choose file?
+    return send_file(io.BytesIO(maps_file), mimetype='image/tiff')
 
-
-@maps_bp.route('/objects/<string:img_id>', methods=['GET'])
-def get_all_objects(img_id):
-    return db.images.find_one(ObjectId(img_id))["objects"] 
