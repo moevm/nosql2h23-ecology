cf7e1d327e98d27c1a78a99a02fddb19195561f9
Vlad <vladbolkunovv@gmail.com>
2023-11-26 17:07:15 +0300

    feat: admin api

---
+++ client/src/components/routes/users/api.ts
@@ -0,0 +1,21 @@
+import { api } from "@/api";
+import { User } from "@/types/users";
+import { UserCreation } from "@/components/routes/users/types";
+
+function getUsers() {
+  return api.get<User[]>("/users");
+}
+
+function createUser(user: UserCreation) {
+  return api.post("/users", user);
+}
+
+function deleteUser(id: string) {
+  return api.delete(`/users/user/${id}`);
+}
+
+function updateUser(id: string, user: UserCreation) {
+  return api.put(`/users/user/${id}`, user);
+}
+
+export const UserAdminAPI = { getUsers, createUser, deleteUser, updateUser };
