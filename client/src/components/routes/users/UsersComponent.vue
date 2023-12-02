b1f0ea38bdf947234802cb6a146b3ab746deb2ed
Vlad <vladbolkunovv@gmail.com>
2023-12-02 15:42:25 +0300

    feat: admin users operations

--- client/src/components/routes/users/UsersComponent.vue
+++ client/src/components/routes/users/UsersComponent.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="container-lg mt-3">
-    <h3>Пользователи</h3>
+    <h3 class="d-flex justify-content-between">
+      Пользователи
+      <button class="btn btn-primary" @click="showAddModal">
+        Добавить
+        <i class="bi bi-plus-circle"></i>
+      </button>
+    </h3>
     <AgGridVue
       class="ag-theme-alpine"
       :column-defs="columnDefs"
@@ -8,6 +14,44 @@
       :grid-options="options"
       @grid-ready="fitActionsColumn"
     />
+
+    <UserDataModal
+      v-if="selected"
+      ref="addUserModal"
+      :user="selected"
+      @submit="onAdd"
+    />
+
+    <UserDataModal
+      v-if="selected"
+      ref="editUserModal"
+      :user="selected"
+      @submit="onEdit"
+    />
+
+    <Modal ref="deleteModal" backdrop="static">
+      <template #header="{ close }">
+        <h4 class="modal-title">Удалить пользователя</h4>
+        <button type="button" class="btn-close" @click="close"></button>
+      </template>
+
+      <template #body>
+        <p>
+          Вы действительно хотите удалить пользователя {{ selected?.name }} ({{
+            selected?.login
+          }}) ?
+        </p>
+      </template>
+
+      <template #footer="{ close }">
+        <button type="button" class="btn btn-secondary" @click="close">
+          Отмена
+        </button>
+        <button type="button" class="btn btn-danger" @click="onRemove">
+          Удалить
+        </button>
+      </template>
+    </Modal>
   </div>
 </template>
 
@@ -20,7 +64,7 @@ import {
   getDefaultGridOptions,
 } from "@/ag-grid/factory";
 import { MapInfo } from "@/types/maps";
-import { onBeforeMount, ref } from "vue";
+import { nextTick, onBeforeMount, ref } from "vue";
 import { User } from "@/types/users";
 import { UserAdminAPI } from "@/components/routes/users/api";
 import { useRouter } from "vue-router";
@@ -28,13 +72,22 @@ import { useUserStore } from "@/store/user";
 import { routeNames } from "@/router";
 import { ToastTypes } from "@/config/toast";
 import { useToaster } from "@/store/toaster";
+import UserDataModal from "@/components/routes/users/UserDataModal.vue";
+import Modal from "@/components/common/Modal.vue";
+import { getEmptyUser } from "@/config/users";
+import _ from "lodash";
 
 const router = useRouter();
+
+const editUserModal = ref<InstanceType<typeof UserDataModal> | null>(null),
+  addUserModal = ref<InstanceType<typeof UserDataModal> | null>(null),
+  deleteModal = ref<InstanceType<typeof Modal> | null>(null);
 
 const userStore = useUserStore(),
   toaster = useToaster();
 
-const users = ref<User[]>([]);
+const users = ref<User[]>([]),
+  selected = ref<User | null>(null);
 
 onBeforeMount(async () => {
   users.value = (await UserAdminAPI.getUsers()).data;
@@ -67,7 +120,8 @@ const columnDefs: ColDef<User>[] = [
         icon: "bi bi-trash",
         button: "btn-danger",
         onClicked: (action, data) => {
-          /**/
+          selected.value = data;
+          nextTick(() => deleteModal.value?.open());
         },
       },
       {
@@ -75,7 +129,8 @@ const columnDefs: ColDef<User>[] = [
         icon: "bi bi-gear",
         button: "btn-warning",
         onClicked: (action, data) => {
-          /**/
+          selected.value = data;
+          nextTick(() => editUserModal.value?.modal?.open());
         },
       },
     ]),
@@ -88,6 +143,35 @@ const options: GridOptions<MapInfo> = {
   domLayout: "autoHeight",
   animateRows: true,
 };
+
+function showAddModal() {
+  selected.value = getEmptyUser();
+  nextTick(() => addUserModal.value?.modal?.open());
+}
+
+async function onAdd(user: User) {
+  const id = (await UserAdminAPI.createUser(_.omit(user, "_id"))).data;
+  users.value.unshift({ ...user, _id: { $oid: id } });
+}
+
+async function onRemove() {
+  if (selected.value) {
+    await UserAdminAPI.deleteUser(selected.value._id.$oid);
+    users.value = users.value.filter(
+      (u) => u._id.$oid !== selected.value?._id.$oid
+    );
+    deleteModal.value?.close();
+  }
+}
+
+async function onEdit(user: User) {
+  await UserAdminAPI.updateUser(user._id.$oid, _.omit(user, "_id"));
+  users.value.splice(
+    users.value.findIndex((u) => u._id.$oid === user._id.$oid),
+    1,
+    user
+  );
+}
 </script>
 
 <style scoped lang="scss"></style>
