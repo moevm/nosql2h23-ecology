f75c726064b7f54010b63d7059ac7991f91296da
Давыдов Михаил <71800457+dart-mih@users.noreply.github.com>
2023-12-09 13:10:43 +0300

    add check in export

--- server/app/routes/objects.py
+++ server/app/routes/objects.py
@@ -133,7 +133,11 @@ class ObjectsImpex(Resource):
 
     def post(self):
         new_objects = json.load(request.files['objects'])
-
+        object_keys = ["_id", "type", "name", "color", "update", "coordinates", "center"]
+        for obj in new_objects:
+            for k in object_keys:
+                if k not in obj:
+                    return "Not ok"
         db.objects.insert_many(new_objects)
         return "Ok"
 
