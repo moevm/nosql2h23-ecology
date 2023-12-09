084d9aeb5dc7d277724d2c52b2d2d79a51ae7365
Vlad <vladbolkunovv@gmail.com>
2023-12-09 17:14:08 +0300

    feat: export

--- client/src/components/routes/objects/api.ts
+++ client/src/components/routes/objects/api.ts
@@ -1,7 +1,11 @@
 import { ObjectInfo } from "@/types/objects";
 import axios from "axios";
-import { baseURL } from "@/api";
+import { api, baseURL } from "@/api";
 
 export async function getObjectsInfo(): Promise<ObjectInfo[]> {
   return (await axios.get<ObjectInfo[]>(baseURL + "/objects/")).data;
+}
+
+export async function exportObjects() {
+  return api.get<ObjectInfo[]>("/objects/impex");
 }
