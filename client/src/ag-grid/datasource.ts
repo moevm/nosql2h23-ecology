1181e00c2534a46479b3ab2a964e63eea0d35525
Vlad <vladbolkunovv@gmail.com>
2024-02-08 01:43:39 +0300

    fix(datasource): log & linted

--- client/src/ag-grid/datasource.ts
+++ client/src/ag-grid/datasource.ts
@@ -1,34 +1,34 @@
-import { api } from '@/api';
-import { IDatasource, IGetRowsParams } from 'ag-grid-community'
+import { api } from "@/api";
+import { IDatasource, IGetRowsParams } from "ag-grid-community";
 import { MapInfo } from "@/types/maps";
 
-
 export class DataSource implements IDatasource {
-    dataFetchURL: string;
+  dataFetchURL: string;
 
-    constructor(dataFetchURL: string) {
-        this.dataFetchURL = dataFetchURL;
-    }
+  constructor(dataFetchURL: string) {
+    this.dataFetchURL = dataFetchURL;
+  }
 
-    getRows(params: IGetRowsParams) {
-        console.log(JSON.stringify(params));
+  getRows(params: IGetRowsParams) {
+    const getParams = {
+      start: params.startRow,
+      end: params.endRow,
+      sort: JSON.stringify(params.sortModel),
+      filter: JSON.stringify(params.filterModel),
+    };
 
-        let getParams = {
-            start: params.startRow,
-            end: params.endRow,
-            sort: JSON.stringify(params.sortModel),
-            filter: JSON.stringify(params.filterModel)
-        }
-
-        api.get<{ rowData: MapInfo[], end: number }>(this.dataFetchURL, {
-            params: getParams,
-        }).then((response) => {
-            let data = response.data;
-            console.log(data);
-            if (data.end === params.endRow) data.end = -1;
-            params.successCallback(data.rowData, data.end);
-        }).catch(() => {
-            params.failCallback();
-        });
-    }
-};
+    api
+      .get<{ rowData: MapInfo[]; end: number }>(this.dataFetchURL, {
+        params: getParams,
+      })
+      .then((response) => {
+        const data = response.data;
+        console.log(data);
+        if (data.end === params.endRow) data.end = -1;
+        params.successCallback(data.rowData, data.end);
+      })
+      .catch(() => {
+        params.failCallback();
+      });
+  }
+}
